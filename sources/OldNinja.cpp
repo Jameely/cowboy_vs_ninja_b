80a64d27fcf21f2bbcbc87e88b16c11eb0228b3d
Jameely <119011959+Jameely@users.noreply.github.com>
2023-05-24 01:05:49 +0300

    Delete sources directory

--- sources/OldNinja.cpp
+++
@@ -1,8 +0,0 @@
-#include "OldNinja.hpp"
-
-namespace ariel{
-    OldNinja::OldNinja(string name ,const Point& location):Ninja(name,location){
-        this->setHitPoints(150);
-        this->setSpeed(8);
-    }
-}
--- sources/YoungNinja.cpp
+++
@@ -1,9 +0,0 @@
-#include "YoungNinja.hpp"
-
-namespace ariel{
-    YoungNinja::YoungNinja(string name ,const Point& location ):Ninja(name,location){
-        this->setHitPoints(100);
-        this->setSpeed(14);
-    }
-    
-}
